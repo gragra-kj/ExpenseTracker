294134d0d083d5dd5a0ce83f119e2215b54c8a24
gragra-kj <muthuigracia27@gmail.com>
2025-07-17 15:35:17 +0300

    views and serializers

---
+++ expensetrackerapi/expenseapi/serializer.py
@@ -0,0 +1,10 @@
+from .models import ExpenseModel
+from rest_framework import serializers
+
+class ExpenseSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=ExpenseModel
+        fields="__all__"
+        read_only_fields=['user','created_at']
+        
+        
--- expensetrackerapi/expenseapi/views.py
+++ expensetrackerapi/expenseapi/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
 
 # Create your views here.
+from .serializer import ExpenseSerializer
+from .models import ExpenseModel
+from rest_framework import viewsets,permissions
+
+class ExpenseViewsets(viewsets.ModelViewSet):
+    queryset=ExpenseModel.objects.all()
+    serializer_class=ExpenseSerializer
+    permission_classes=[permissions.IsAuthenticatedOrReadOnly]
+    
+    
+    def get_queryset(self):
+        return ExpenseModel.objects.filter(user=self.request.user).order_by('-date')
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
