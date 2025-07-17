dc210b8760aa3f6c9cadffed8dbf6cb737a16e55
gragra-kj <muthuigracia27@gmail.com>
2025-07-17 16:27:29 +0300

    routes

--- expensetrackerapi/expenseapi/serializer.py
+++ expensetrackerapi/expenseapi/serializer.py
@@ -5,6 +5,7 @@ class ExpenseSerializer(serializers.ModelSerializer):
     class Meta:
         model=ExpenseModel
         fields="__all__"
-        read_only_fields=['user','created_at']
+        read_only_fields = ["user", "created_at"]
+
         
         
